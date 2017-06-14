54ec22ac2fc6e803093715b3004838ec3c1043cc
Safi.Ullah <safi.ullah@arbisoft.com>
2017-06-14 12:01:09 +0500

    source file changed

--- source.py
+++ source.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     q1 = Quizzer()
     q1.read_questions_with_key("./questions.txt", "./key.txt")
 
-    # generate_quiz takes no. of questions as argument
+    # start_quiz takes no. of questions as argument
     q1.start_quiz(2)
 
     q1.generate_result()
